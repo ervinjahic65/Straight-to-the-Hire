5e73b3f3f984a71346dee26a488f34a30ef1e51d
ervinjahic65 <ervinjahic65@gmail.com>
2025-04-16 12:40:11 +0200

    initial commit

---
+++ Hire Independent Contractors Globally - Contractor Employment Contract _ Deel Contractor_files/geo4.js
@@ -0,0 +1,17 @@
+(function(){
+  window['optimizely'] = window['optimizely'] || [];
+  window['optimizely'].push(['activateGeoDelayedExperiments', {
+    'location':{
+      'city': "SARAJEVO",
+      'continent': "EU",
+      'country': "BA",
+      'region': "BIH",
+      'dma': ""
+    },
+    'ip':"109.237.32.222"
+  }]);
+})
+
+()
+
+;
